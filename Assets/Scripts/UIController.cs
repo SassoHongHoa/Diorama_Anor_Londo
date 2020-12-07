7c09b4ef4b018a7fb17e64cbca50fd3a266114a4
Salomé Duboscq <salome.duboscq@gmail.com>
2020-12-07 22:36:11 +0100

    UI control

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
     public GameObject ControlsParent;
+    public RawImage Background;
 
     public void OnActiveControlsUI()
     {
         ControlsParent.SetActive(!ControlsParent.activeSelf);
+        Background.enabled = ControlsParent.activeSelf;
     }
 }
