146ebae45c26db236022da2ad32cde7b7b5058f2
Salomé Duboscq <salome.duboscq@gmail.com>
2020-12-07 17:38:45 +0100

    reduce menu

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject ControlsParent;
+
+    public void OnActiveControlsUI()
+    {
+        ControlsParent.SetActive(!ControlsParent.activeSelf);
+    }
+}
